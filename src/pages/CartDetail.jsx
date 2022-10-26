1c95d4c597d24098df2bf0f96c5aea1b92f6a5a6
Achsmboh <maulana.achsanxiipa1@gmail.com>
2022-10-26 20:58:53 +0700

    integrasi api cart detail

--- src/pages/CartDetail.jsx
+++ src/pages/CartDetail.jsx
@@ -3,18 +3,52 @@ import Layout from "../components/Layout";
 import { CardCart } from "../components/Card";
 import { CustomButtonSatu } from "../components/CustomButton";
 import { useTitle } from "../utils/redux/useTitle";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { handleAuth } from "../utils/redux/reducers/reducer";
+import Skeleton from "react-loading-skeleton";
 
 function CartDetail() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [datas, setDatas] = useState([]);
+
   useTitle("My Cart");
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    axios
+      .get("https://virtserver.swaggerhub.com/HERIBUDIYANA/E-Commerce/1.0.0/carts", {})
+      .then((res) => {
+        const { data } = res;
+        setDatas(data);
+      })
+      .catch((err) => {
+        const { data } = err.response;
+        if ([401, 403].includes(data.code)) {
+          localStorage.removeItem("token");
+          dispatch(handleAuth(false));
+          navigate("/login");
+        }
+        alert(data.message);
+      })
+      .finally(() => setLoading(false));
+  };
+  console.log(datas);
+
   return (
     <Layout>
-      <div className="flex flex-row fonr-poppins">
-        <div className="basis-3/4">
-          <div>
-            <CardCart />
-          </div>
+      <div className="lg:flex flex-row fonr-poppins">
+        <div className="lg:basis-3/4">
+          <div>{loading ? <Skeleton /> : datas.myCart.map((datum) => <CardCart images={datum.images} name={datum.name} address={datum.address} price={datum.price} qti={datum.qty} />)}</div>
         </div>
-        <div className="basis-1/4 shadow-xl">
+        <div className="lg:basis-1/4 shadow-xl">
           <div className="flex flex-col justify-between w-full border h-full items-center p-5">
             <div>
               <h2 className="text-2xl font-bold">Prise Total</h2>
